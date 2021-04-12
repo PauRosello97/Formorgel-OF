35be1386d96832382366c743254c4f017f8a05b0
Pau Rosello <pau.rosello@itequia.com>
2021-04-12 22:57:59 +0200

    Classes config

--- Osc.cpp
+++ Osc.cpp
@@ -1,1 +1,11 @@
 #include "Osc.h"
+
+void Osc::display() {
+	//button.display();
+	//angleInput.display();
+	//ofDrawBitmapString("Offset: " + ofToString(offset), x+100, y+100);
+	//ofDrawBitmapString("Length: " + ofToString(length), x+100, y+120);
+	ofDrawBitmapString("Angle: " + ofToString(angle), x + 100, y + 140);
+	ofSetColor(255, 255, 255);
+	ofDrawRectangle(x, y, w, h);
+}
--- Osc.h
+++ Osc.h
@@ -1,14 +1,25 @@
 #pragma once
+#include "ofMain.h"
 #include "FloatInput.h"
 
 class Osc
 {
     FloatInput b;
+    float x, y, h, w;
     float value;
+    float angle = ofRandom(90);
 
 public:
-    Osc() : value(50), b(value)
-    {}
+    Osc() : value(50), b(value) {};
+    Osc(float _x, float _y, float _w, float _h) : value(50), b(value)
+    {
+        x = _x;
+        y = _y;
+        w = _w;
+        h = _h;
+    }
+
+    void display();
 
     void modify_value() {
         b.modify_value();
