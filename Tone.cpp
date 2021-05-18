168b7410894dbbf529207c600e3b3b1cd78da30a
Pau Rosello <pau.rosello@itequia.com>
2021-05-18 22:59:58 +0200

    HLuv coding

--- Tone.cpp
+++ Tone.cpp
@@ -1,1 +1,15 @@
 #include "Tone.h"
+
+ofColor Tone::getColor() {
+	ofColor c;
+	c.setHsb(hue, 255, luminance * 2.5);
+	return c;
+}
+
+void Tone::increaseTone() {
+	number = number == 6 ? 0 : number + 1;
+}
+
+void Tone::decreaseTone() {
+	number = number == 0 ? 6 : number - 1;
+}
--- Tone.h
+++ Tone.h
@@ -13,16 +13,8 @@ public:
 	float hue;
 	float luminance;
 
-	ofColor getColor() {
-		ofColor c;
-		c.setHsb(hue, 255, luminance*2.5);
-		return c;
-	}
-	void increaseTone() {
-		number = number == 6 ? 0 : number + 1;
-	}
-	void decreaseTone() {
-		number = number == 0 ? 6 : number - 1;
-	}
+	ofColor getColor();
+	void increaseTone();
+	void decreaseTone();
 };
 
