59a035ffa895afeea6e74ed09fd446b56108dad2
Pau Rosello <pau.rosello@itequia.com>
2021-05-20 14:18:17 +0200

    ofxPolygonDetector update (reset)

--- src/GeometricSynth.cpp
+++ src/GeometricSynth.cpp
@@ -4,12 +4,10 @@ GeometricSynth::GeometricSynth(){}
 
 void GeometricSynth::generateComposition(Formorgel fA, Formorgel fB, Formorgel fC, int iterations) {
 	generateLines(fA, fB, fC, iterations);
-
-	polyDetector.reset();
+	
 	polyDetector.detectPolygons();
 
 	polygons = polyDetector.polys;
-	//polygons.at(2).setColor(ofColor(255, 0, 0));
 }
 
 void GeometricSynth::generateLines(Formorgel fA, Formorgel fB, Formorgel fC, int iterations) {
@@ -33,7 +31,7 @@ void GeometricSynth::display() {
 }
 
 void GeometricSynth::displayLines() {
-	ofSetColor(0, 0, 0);
+	ofSetColor(0);
 	for (int i = 0; i < lines.size(); i++) {
 		ofDrawLine(lines.at(i).a.x, lines.at(i).a.y, lines.at(i).b.x, lines.at(i).b.y);
 	}	
