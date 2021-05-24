a4d45ab95ac086777aef4a4e9debe4511237c07c
Pau Rosello <paurosellodiaz@gmail.com>
2021-05-24 19:47:21 +0200

    Selector h->cpp

--- src/Selector.h
+++ src/Selector.h
@@ -16,43 +16,10 @@ class Selector
 public:
 	
 	Selector() {};
-	Selector(float _x, float _y, vector<string> _valueNames) {
-		x = _x;
-		y = _y;
-		previousButton = SmallButton(x+h/2, y+h/2, h-20);
-		nextButton = SmallButton(x+w-h/2, y+h/2, h-20);
-		valueNames = _valueNames;
-		nValues = valueNames.size();
-	};
+	Selector(float _x, float _y, vector<string> _valueNames);
 
-	void display() {
-		// Text
-		ofSetColor(0);
-		ofDrawBitmapString(ofToString(value) + " " + valueNames[value], x + 50, y + 30);
-
-		// Buttons
-		previousButton.display();
-		nextButton.display();
-
-		// Background
-		ofSetColor(200);
-		ofDrawRectangle(x, y, w, h);
-	}
-
-	bool mousePressed() {
-		if (previousButton.isOver()) {
-			value = value > 0 ? value - 1 : (nValues-1);
-			return true;
-		}
-		else if (nextButton.isOver()) {
-			value = (value + 1) % nValues;
-			return true;
-		}
-		return false;
-	}
-
-	int getValue() {
-		return value;
-	}
+	void display();
+	bool mousePressed();
+	int getValue() { return value; }
 };
 
